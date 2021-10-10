b45886f8f11e13a2cb43f8492cdf5b5b1b61528e
Mike Rankin <mrankin@oldgrognard.pub>
2021-10-10 17:55:38 -0400

    process routines

--- scripts/mysqldoc/routines/mysqldoc_procedures.sql
+++ scripts/mysqldoc/routines/mysqldoc_procedures.sql
@@ -37,27 +37,14 @@ begin
     routineloop:
     loop
 
-        fetch routine_cursor into
-            _pname,
-            _routine_definition,
-            _is_deterministic,
-            _sql_data_access,
-            _security_type,
-            _created,
-            _last_altered,
-            _sql_mode,
-            _routine_comment,
-            _definer,
-            _character_set_client,
-            _collation_connection,
-            _database_collation;
+        fetch routine_cursor into _pname, _routine_definition, _is_deterministic, _sql_data_access, _security_type, _created, _last_altered, _sql_mode, _routine_comment, _definer, _character_set_client, _collation_connection, _database_collation;
 
         if _routine_cursor_finished = 1 then leave routineloop; end if;
 
         call mysqldoc_line('procedure', _pname, '[index.md](index.md)');
         call mysqldoc_line('procedure', _pname, concat('# Procedure: ', _pname));
 
-        if _routine_comment <> '' then call mysqldoc_line('procedure', _pname, concat('>',_routine_comment)); end if;
+        if _routine_comment <> '' then call mysqldoc_line('procedure', _pname, concat('>', _routine_comment)); end if;
 
         call mysqldoc_line('procedure', _pname, '');
 
@@ -70,26 +57,39 @@ begin
         call mysqldoc_line('procedure', _pname, concat('| Security Type | ', _security_type, ' |'));
         call mysqldoc_line('procedure', _pname, concat('| Created | ', _created, ' |'));
         call mysqldoc_line('procedure', _pname, concat('| Last Update | ', _last_altered, ' |'));
-        call mysqldoc_line('procedure', _pname, concat('| SQL Mode | ', replace(_sql_mode,',',', '), ' |'));
+        call mysqldoc_line('procedure', _pname, concat('| SQL Mode | ', replace(_sql_mode, ',', ', '), ' |'));
+        call mysqldoc_line('procedure', _pname, concat('| Definer | ', _definer, ' |'));
+        call mysqldoc_line('procedure', _pname, concat('| Client Character Set | ', _character_set_client, ' |'));
+        call mysqldoc_line('procedure', _pname,
+                           concat('| <nobr>Connection Collation</nobr> | ', _collation_connection, ' |'));
+        call mysqldoc_line('procedure', _pname, concat('| Database Collation | ', _database_collation, ' |'));
 
+        set @param_count = ( select count(*)
+                            from information_schema.PARAMETERS
+                            where SPECIFIC_SCHEMA = database()
+                              and SPECIFIC_NAME = _pname
+                            order by ORDINAL_POSITION );
 
-#         call mysqldoc_line('table', tname, '| Timing | Event | Definer | Character Set | Collation | Created |');
-#         call mysqldoc_line('table', tname, '| ------ | ----- | ------- | ------------- | --------- | ------- |');
-#
-#         insert into tmp_docs (type, name, line)
-#         select 'table',
-#                tname,
-#                concat('| ', v_action_timing, ' | ', v_event_manipulation, ' | ', v_definer, ' | ',
-#                       v_character_set_client, ' | ', v_collation_connection, ' | ', v_created, ' |');
+        if @param_count > 0 then
+            call mysqldoc_line('procedure', _pname, '## Parameters');
+            call mysqldoc_line('procedure', _pname, '| Name | Mode | Data Type');
+            call mysqldoc_line('procedure', _pname, '| ---- | ---- | ---------');
 
-#         call mysqldoc_line('table', tname, '');
-#         call mysqldoc_line('table', tname, '```sql');
-#         call mysqldoc_line('table', tname, replace(v_action_statement, '\n', '\r'));
-#         call mysqldoc_line('table', tname, '```');
+            insert into mysqldoc_temp_docs (type, name, line)
+            select 'procedure', _pname, concat('| ', PARAMETER_NAME, ' | ', PARAMETER_MODE, ' | ', DTD_IDENTIFIER, ' |')
+            from information_schema.PARAMETERS
+            where SPECIFIC_SCHEMA = database()
+              and SPECIFIC_NAME = _pname
+            order by ORDINAL_POSITION;
+        end if;
+
+        call mysqldoc_line('procedure', _pname, '## Body');
+        call mysqldoc_line('procedure', _pname, '```sql');
+        call mysqldoc_line('procedure', _pname, replace(_routine_definition, '\n', '\r'));
+        call mysqldoc_line('procedure', _pname, '```');
 
     end loop routineloop;
     close routine_cursor;
-
 end$$
 
 delimiter ;
